12e2b1130402c9fd195afe738c4835208818a92c
sevauribe10-alt <sevauribe10@gmail.com>
2026-09-17 16:00:10 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lluvia-panel-v1';
+const CACHE_NAME = 'lluvia-panel-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
